010d3a748357a02d775dfa4156a50147744c157c
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-07-05 14:53:32 +0530

    feat: implement initial dashboard page and clerk authentication integration for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 export default function Home() {
   return (
-   <SignInButton/>
+    <>
+    <SignInButton />
+    <UserButton />
+    </>
   );
 }
